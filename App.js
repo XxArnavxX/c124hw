b30bf244747d9e3b23d87ce00a715ded9ce744f4
XxArnavxX <60574509+XxArnavxX@users.noreply.github.com>
2021-06-22 23:20:37 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import PickIMG from './screens/camera'
+import PickIMG from '../camera'
 
 export default class App extends React.Component{
   render(){
